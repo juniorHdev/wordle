70f4b41d67fa1fd3e8424b7bbe9c1e71e4e33f74
juniorHdev <jrsalguedo179@gmail.com>
2023-10-09 17:52:55 -0500

    fix: implement context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { GameContext } from "./components/context/GameContext";
 import Table from "./components/table/Table";
 
 function App() {
   return (
     <>
-      <div className="board">
-        <Table/>
-      </div>
+      <GameContext>
+        <div className="board">
+          <h1>juego del wordle</h1>
+          <Table />
+        </div>
+      </GameContext>
     </>
   );
 }
